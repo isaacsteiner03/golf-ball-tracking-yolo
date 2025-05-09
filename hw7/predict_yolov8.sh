28c6651d1093c1fd509f8e1e6783467ae195a63b
Isaac Steiner <isteiner03@ksu.edu>
2025-05-09 13:13:51 -0500

    Remove HW7 Folder

--- hw7/predict_yolov8.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Run inference using the trained YOLOv8n model
-
-yolo task=detect mode=predict \
-  model=yolov8/models/yolov8n_best.pt \
-  source=yolov8/data/test_images/ \
-  conf=0.25
--- hw7/train.yolov8.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Train YOLOv8n model on golf ball dataset
-
-yolo task=detect mode=train \
-  model=yolov8n.pt \
-  data=yolov8/data/data.yaml \
-  epochs=50 \
-  imgsz=640 \
-  name=golf-yolov8
